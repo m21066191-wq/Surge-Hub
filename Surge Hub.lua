2bded90d8321ac54555787d8b183c9e84180b144
m21066191-wq <m21066191@gmail.com>
2026-07-26 07:09:18 -0400

    Update Surge Hub.lua

--- Surge Hub.lua
+++ Surge Hub.lua
@@ -2,13 +2,22 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local MarketplaceService = game:GetService("MarketplaceService")
 
-local TARGET_PLACE_ID = 137233438285284
+local TARGET_PLACE_ID_1 = 137233438285284
+local TARGET_PLACE_ID_2 = 537413528
 
 local currentPlaceId = game.PlaceId
 
-if currentPlaceId == TARGET_PLACE_ID then
+if currentPlaceId == TARGET_PLACE_ID_1 then
     local success, err = pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/m21066191-wq/Surge-Hub/main/Scripts/Chicken%20Farm.lua"))()
+    end)
+    
+    if not success then
+        warn("Surge Hub: Failed to load script -> " .. tostring(err))
+    end
+elseif currentPlaceId == TARGET_PLACE_ID_2 then
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/m21066191-wq/Surge-Hub/main/Scripts/Build%20a%20Boat.lua"))()
     end)
     
     if not success then
