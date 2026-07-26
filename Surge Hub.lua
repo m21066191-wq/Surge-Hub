2bbe17e7c4f88c0309deaee22815b0b8a03d1cb8
m21066191-wq <m21066191@gmail.com>
2026-07-25 23:55:39 -0400

    Update Surge Hub.lua

--- Surge Hub.lua
+++ Surge Hub.lua
@@ -3,13 +3,12 @@ local LocalPlayer = Players.LocalPlayer
 local MarketplaceService = game:GetService("MarketplaceService")
 
 local TARGET_PLACE_ID = 137233438285284
-local SCRIPT_URL = "loadstring(game:HttpGet("https://raw.githubusercontent.com/m21066191-wq/Surge-Hub/main/Scripts/Chicken%20Farm.lua"))()"
 
 local currentPlaceId = game.PlaceId
 
 if currentPlaceId == TARGET_PLACE_ID then
     local success, err = pcall(function()
-        loadstring(game:HttpGet(SCRIPT_URL))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/m21066191-wq/Surge-Hub/main/Scripts/Chicken%20Farm.lua"))()
     end)
     
     if not success then
